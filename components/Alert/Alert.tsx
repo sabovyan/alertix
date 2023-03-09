777c690e893abf26473aa07717681bbd6cb7e56c
sabovyan <abovyan.sargis90@gmail.com>
2023-03-09 10:37:52 +0400

    fix types

--- components/Alert/Alert.tsx
+++ components/Alert/Alert.tsx
@@ -1,19 +1,18 @@
-import {  MouseEventHandler, ReactNode } from 'react';
+import { MouseEventHandler, ReactNode } from 'react';
 import { notionColors } from '@/helpers/notion.helper';
-
 
 export type AlertAction = {
   title: string;
   handler: (id: string) => MouseEventHandler<HTMLButtonElement>;
-}
+};
 
 export type AlertProps = {
   content: ReactNode;
   action?: AlertAction;
   id: string;
-}
+};
 
-export function Alert({ content, action, id }: AlertProps) {
+export function Alert({ content, id, action }: AlertProps) {
   return (
     <div
       style={{
@@ -24,11 +23,15 @@ export function Alert({ content, action, id }: AlertProps) {
         marginBlockEnd: '0.5em',
       }}
     >
-        <span>{content}</span>
+      <span>{content}</span>
 
-      <div style={{display: 'flex', gap: '1em'}}>
-        {action ? <button onClick={action.handler(id)} style={{border: '1px solid'}}>{action.title}</button> : null}
-      <span >x</span>
+      <div style={{ display: 'flex', gap: '1em' }}>
+        {action ? (
+          <button onClick={action.handler(id)} style={{ border: '1px solid' }}>
+            {action.title}
+          </button>
+        ) : null}
+        <span>x</span>
       </div>
     </div>
   );
